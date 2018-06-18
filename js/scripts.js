7be4a1e05a2eb7f315cd71ca7e639270c505e673
visetty <visalalakkimsetty@gmail.com>
2018-06-18 01:25:58 -0700

    added scripts in js to the grocery page

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,19 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
     var item1Input = $("input#item1").val();
+    $("#item1").val('');    
     var item2Input = $("input#item2").val();
+    $("#item2").val('');
     var listItems = [item1Input, item2Input]
     listItems.sort();
-    console.log(listItems);
+
+
+    $("#list-item1").html('');
 
     listItems.forEach(function(listItem){
-      $("#list-item1").append("<li>" + listItem + "</li>");      
-    })
+      //var x = listItem.toUpperCase();
+      $("#list-item1").append("<li>" + listItem.toUpperCase() + "</li>");
+    });
      $(".list").show();
     event.preventDefault();
   });
